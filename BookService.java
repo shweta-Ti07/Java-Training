18fe31b3108499085e6b7d66b2390613ab5548c1
Shweta <161307494+shweta-Ti07@users.noreply.github.com>
2026-07-30 10:04:43 +0530

    springboot

---
+++ BookService.java
@@ -0,0 +1,14 @@
+package com.coforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day160726SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day160726SpringBootApplication.class, args);
+		System.out.println("SpringBoot Application");
+	}
+
+}
